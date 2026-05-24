8e6ddfbaeac3e57525c590b1e0c17ee8b87deac2
unknown <AgrawalDiva>
2026-05-24 08:38:26 +0530

    Added reversearray.cpp

---
+++ array/reversearray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int arr[5];
+    for (int i=0;i<5;i++){
+        cout<< "Enter the number for array " << i+1 << ":";
+        cin>> arr[i];
+    }
+    cout<< "Array initially"<< endl;
+    for (int i=0;i<5;i++){
+        cout<< arr[i]<< endl;
+    }
+    cout<<"Reverse of array"<<endl;
+    for (int i=4;i>=0;i--){
+        cout<< arr[i]<< endl;
+    }
+}
